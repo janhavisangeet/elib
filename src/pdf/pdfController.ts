ea3d88689f6722b7b363bdbe6fc087a8a0af2fda
janhavi <sangeetjanhavi@gmail.com>
2025-04-11 16:42:58 +0530

    comment code

--- src/pdf/pdfController.ts
+++ src/pdf/pdfController.ts
@@ -69,7 +69,7 @@ const createPdf = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const updatePdf = async (req: Request, res: Response, next: NextFunction) => {
-    const { title, description, genre } = req.body;
+    //const { title, description, genre } = req.body;
     const pdfId = req.params.pdfId;
 
     const pdf = await pdfModel.findOne({ _id: pdfId });
@@ -86,25 +86,27 @@ const updatePdf = async (req: Request, res: Response, next: NextFunction) => {
     // check if image field is exists.
 
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-    let completeCoverImage = "";
-    if (files.coverImage) {
-        const filename = files.coverImage[0].filename;
-        const converMimeType = files.coverImage[0].mimetype.split("/").at(-1);
-        // send files to cloudinary
-        const filePath = path.resolve(
-            __dirname,
-            "../../public/data/uploads/" + filename
-        );
-        completeCoverImage = filename;
-        const uploadResult = await cloudinary.uploader.upload(filePath, {
-            filename_override: completeCoverImage,
-            folder: "pdf-covers",
-            format: converMimeType,
-        });
 
-        completeCoverImage = uploadResult.secure_url;
-        await fs.promises.unlink(filePath);
-    }
+    
+    // let completeCoverImage = "";
+    // if (files.coverImage) {
+    //     const filename = files.coverImage[0].filename;
+    //     const converMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    //     // send files to cloudinary
+    //     const filePath = path.resolve(
+    //         __dirname,
+    //         "../../public/data/uploads/" + filename
+    //     );
+    //     completeCoverImage = filename;
+    //     const uploadResult = await cloudinary.uploader.upload(filePath, {
+    //         filename_override: completeCoverImage,
+    //         folder: "pdf-covers",
+    //         format: converMimeType,
+    //     });
+
+    //     completeCoverImage = uploadResult.secure_url;
+    //     await fs.promises.unlink(filePath);
+    // }
 
     // check if file field is exists.
     let completeFileName = "";
