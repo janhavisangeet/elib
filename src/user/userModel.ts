24e623dea4d063e36cdd4b8922931a6514969a92
janhavi <sangeetjanhavi@gmail.com>
2025-04-18 13:48:12 +0530

    add role in user schema

--- src/user/userModel.ts
+++ src/user/userModel.ts
@@ -2,22 +2,28 @@ import mongoose from "mongoose";
 import { User } from "./userTypes";
 
 const userSchema = new mongoose.Schema<User>(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: ["USER", "ADMIN"],
+            default: "USER",
+            required: true,
+        },
     },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export default mongoose.model<User>("User", userSchema);
